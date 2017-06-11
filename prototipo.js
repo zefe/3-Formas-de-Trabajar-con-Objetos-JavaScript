a2c3cdcfc24b56795b635e2c0906a3fcb4f225a7
zefetorres <zefetorres@icloud.com>
2017-06-10 23:12:52 -0500

    obj create

--- prototipo.js
+++ prototipo.js
@@ -1,3 +1,4 @@
+/*
 function Punto(x,y){ //Constructor
 	this.x = x
 	this.y = y
@@ -18,9 +19,35 @@ Punto.prototype.distancia = function distancia(p){
 	return Math.sqrt(x * x + y * y)
 
 }
+*/
 
-const p1 = new Punto(0, 4)
-const p2 = new Punto(3, 0)
+const Punto = {
+	init: function (x, y){
+		this.x = x
+		this.y = y
+	},
+	moverEnX: function moverEnX(x){
+		this.x += x
+	},
+	moverEnY: function moverEnY(y){
+		this.y += y
+	},
+	distancia: function distancia(p){
+		const x = this.x - p.x 
+		const y = this.y - p.y
+
+		return Math.sqrt(x * x + y *y)
+	}
+}
+
+//const p1 = new Punto(0, 4)
+//const p2 = new Punto(3, 0)
+
+const p1 = Object.create(Punto)
+const p2 = Object.create(Punto)
+
+p1.init(0, 4)
+p2.init(3, 0)
 
 console.log(p1.distancia(p2))
 console.log(p2.distancia(p1))
